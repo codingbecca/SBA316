0341de6bc96d4142e78b653c3a082718f55d2920
codingbecca <131915898+codingbecca@users.noreply.github.com>
2025-02-06 06:15:29 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// todo: Cache at least one element using selectElementById.
+// todo: Cache at least one element using querySelector or querySelectorAll.
+// todo: Use the parent-child-sibling relationship to navigate between elements at least once (firstChild, lastChild, parentNode, nextElementSibling, etc.).
+// todo: Iterate over a collection of elements to accomplish some task.
+// todo: Create at least one element using createElement.
+// todo: Use appendChild and/or prepend to add new elements to the DOM.
+// todo: Use the DocumentFragment interface or HTML templating with the cloneNode method to create templated content.
+// todo: Modify the HTML or text content of at least one element in response to user interaction using innerHTML, innerText, or textContent.
+// todo: Modify the style and/or CSS classes of an element in response to user interactions using the style or classList properties. 
+// todo: Modify at least one attribute of an element in response to user interaction.
+// todo: Register at least two different event listeners and create the associated event handler functions.
+// todo: Use at least two Browser Object Model (BOM) properties or methods.
+// todo: Include at least one form and/or input with HTML attribute validation.
+// todo: Include at least one form and/or input with DOM event-based validation. (This can be the same form or input as the one above, but should include event-based validation in addition to the HTML attribute validation.)
